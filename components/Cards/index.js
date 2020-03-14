60d0a4dbaba9aa63a8ca3695617c1f10d8a52eb3
Shawn Rice <ryse5350@gmail.com>
2020-03-14 10:49:09 -0400

    Completed mvp, not like it took me forever :')

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,67 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function cardCreator(data) {
+    const cardContainer = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const authImg = document.createElement('img');
+    const name = document.createElement('span');
+
+    cardContainer.append(headline);
+    cardContainer.append(author);
+
+    author.append(imgContainer);
+    author.append(name);
+
+    imgContainer.append(authImg);
+
+    cardContainer.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    headline.textContent = `${data.headline}`;
+    authImg.setAttribute('src', `${data.authorPhoto}`);
+    name.textContent = `By ${data.authorName}`;
+
+
+    return cardContainer;
+}
+
+const cardsCont = document.querySelector('.cards-container');
+
+axios
+    .get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then(response => {
+        console.log(response);
+        console.log(response.data);
+
+        const articleData = response.data.articles;
+        articleData.javascript.forEach(el => {
+            const card = cardCreator(el);
+            cardsCont.append(card);
+        })
+        articleData.bootstrap.forEach(el => {
+            const card = cardCreator(el);
+            cardsCont.append(card);
+        })
+        articleData.technology.forEach(el => {
+            const card = cardCreator(el);
+            cardsCont.append(card);
+        })
+        articleData.jquery.forEach(el => {
+            const card = cardCreator(el);
+            cardsCont.append(card);
+        })
+        articleData.node.forEach(el => {
+            const card = cardCreator(el);
+            cardsCont.append(card);
+        })
+    })
+
+    .catch(error => {
+        console.log(`Error encountered.`, error)
+    });
